8ea4dbbf1ea9878ad0196c5e1989899495a5ad51
juliana geller <bablouchka@gmail.com>
2019-11-08 10:17:42 +0100

    add space

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :doses, only: [ :new, :create ]
   end
   resources :doses, only: [ :show, :edit, :update, :destroy ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
